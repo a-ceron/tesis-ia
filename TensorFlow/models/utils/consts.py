0085c20c34fd9c2aea7c68c12be1b2083c6c9b48
aceron <ariceg@comunidad.unam.mx>
2022-12-19 21:59:55 -0600

    TF: VGG final changes

---
+++ TensorFlow/models/utils/consts.py
@@ -0,0 +1,14 @@
+IMAGE_SIZE_100 = [100, 100]
+
+FIVE_EPOCHS = 5
+TEN_EPOCHS = 10
+
+THITTY_TWO_BATCH_SIZE = 32
+SIXTY_FOUR_BATCH_SIZE = 64
+ONE_HUNDRED_AND_TWENTY_EIGHT_BATCH_SIZE = 128
+
+TRAINI_SIZE = 0.8
+
+RANDOM_STATE = 42
+
+DATA_PATH = '/Users/aceron/Library/CloudStorage/GoogleDrive-arielcerong@gmail.com/Mi unidad/Aprendizaje/Tesis/Maestría/Code/master-degree/TensorFlow/data'
