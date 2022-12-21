23068c051f0f8ca44551ba4849852428e0ee990e
aceron <ariceg@comunidad.unam.mx>
2022-12-20 22:43:38 -0600

    TF: Add ResNet model

--- TensorFlow/main.py
+++ TensorFlow/main.py
@@ -1,4 +1,4 @@
-from models import VGG
+from models import VGG, ResNet
 
 def VGG16():
     vgg = VGG.My_VGG16()
@@ -6,10 +6,15 @@ def VGG16():
     vgg.model()
     vgg.train()    
     
+def ResNet50():
+    resnet = ResNet.My_ResNet()
 
+    resnet.model()
+    resnet.train()
+    
 
 def main():
-    VGG16()
+   ResNet50()
 
 if __name__ == '__main__':
     main()
