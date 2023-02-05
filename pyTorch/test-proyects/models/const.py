a5886fea09c3740cdeff9a1b11a182b6f5ce9a57
a-ceron <ariceg@comunidad.unam.mx>
2023-02-04 21:41:08 -0600

    Remove files

--- pyTorch/test-proyects/models/const.py
+++
@@ -1,9 +0,0 @@
-BATCH_SIZE_256 = 256
-BATCH_SIZE_128 = 128
-BATCH_SIZE_64 = 64
-BATCH_SIZE_32 = 32
-
-TRAIN = 0.3
-
-PATH_OF_FIGURES = '/home/ariel/Documents/models-aceron/lens-data/figures'
-PATH_OF_TSV = '/home/ariel/Documents/models-aceron/lens-data/utils/training_lens.tsv'
