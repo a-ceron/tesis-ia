72948989d1f8f2a6af548cf7c6bc4b5164f3c2ea
a-ceron <ariceg@comunidad.unam.mx>
2023-03-25 19:41:52 -0600

    New gans models

---
+++ lens_effect_project/app/model/CNN/CNN.py
@@ -0,0 +1,15 @@
+"""
+###########################################
+Esta es una implementación de una arquitecura
+CNN
+
+
+
+autor: Ariel Cerón González
+fecha: Marzo 14, 2023
+
+Bajo la tutoria del Dr. Gibran Fuentes
+IIMAS, UNAM
+###########################################
+"""
+
